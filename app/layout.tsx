12957e741534f90468f8a096d3c2026e7bcd39d1
Anil Pandiya <anilpandiya05@gmail.com>
2023-12-21 11:37:34 +0530

    FE | speed insights changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-black`}>{children}</body>
+      <body className={`${inter.className} bg-black`}>
+        {children} 
+        <SpeedInsights />
+      </body>
     </html>
   )
 }
